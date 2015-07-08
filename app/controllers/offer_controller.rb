33b981bb0082407e01a2b2bf040623fe2740ce6b
John Abraham <jef.abraham@gmail.com>
2015-07-08 17:43:14 +0530

    Removed some 'puts'.

--- app/controllers/offer_controller.rb
+++ app/controllers/offer_controller.rb
@@ -2,9 +2,7 @@ class OfferController < ApplicationController
 
   def index
     if params['uid']
-      puts "here>>"
       @offers = Offer.get(params)
-      puts @offer.inspect
     end
   end
     
